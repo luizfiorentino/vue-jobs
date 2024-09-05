bb028f9a0e55580947d06362989d330657d7f33e
luizfiorentino <lgfiorentino@gmail.com>
2024-09-05 11:37:21 +0200

    added add and delete task buttons

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { ref } from "vue";
 const name = ref("John Doe");
 const status = ref("active");
 const tasks = ref(["Task one", "Task two", "Task three"]);
+const newTask = ref("");
 
 const toggleStatus = () => {
   if (status.value === "active") {
@@ -14,17 +15,38 @@ const toggleStatus = () => {
     status.value = "active";
   }
 };
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
 </script>
 <template>
   <h1>{{ name }}</h1>
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
-  <a v-bind:href="link">Click for google</a>
+  <!-- <a v-bind:href="link">Click for google</a> -->
   <br />
   <!-- <button v-on:click="toggleStatus">Change status</button> -->
   <button @click="toggleStatus">Change status</button>
